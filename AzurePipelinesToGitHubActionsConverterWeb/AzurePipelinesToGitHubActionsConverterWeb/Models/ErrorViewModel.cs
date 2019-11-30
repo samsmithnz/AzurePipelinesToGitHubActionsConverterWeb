f444409357adb486abcec5c78949f6d4cf862559
Sam Smith <samsmithnz@gmail.com>
2019-11-30 07:46:41 -0500

    Pushing intial website with application insights and publish profile

---
+++ AzurePipelinesToGitHubActionsConverterWeb/AzurePipelinesToGitHubActionsConverterWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzurePipelinesToGitHubActionsConverterWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
