367af617841f6e839599037a60b29e3e06c2546c
Sam Smith <samsmithnz@gmail.com>
2019-11-30 22:09:54 -0500

    Added wireframes and more formatting to the front page

--- PipelinesToActions/PipelinesToActions/wwwroot/js/site.js
+++ PipelinesToActions/PipelinesToActions/wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function selectElementContentsById(elementName) {
+
+    var el = document.getElementById(elementName);
+
+    // Copy textarea, pre, div, etc.
+    if (document.body.createTextRange) {
+        // IE
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.select();
+        textRange.execCommand("Copy");
+    }
+    else if (window.getSelection && document.createRange) {
+        // non-IE
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+            console.log('Copy command was ' + msg);
+        } catch (err) {
+            console.log('Oops, unable to copy');
+        }
+    }
+} // end function selectElementContents(el)
