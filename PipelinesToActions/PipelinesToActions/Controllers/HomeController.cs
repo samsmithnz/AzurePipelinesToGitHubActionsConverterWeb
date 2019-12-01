b524321df812769a5d7e4418cc5ef30c98c8219a
Sam Smith (Microsoft) <samsmithnz@gmail.com>
2019-12-01 13:41:24 -0500

    Upgrade to zero point three (#1)
    * Upgrade to 0.3 core class
    
    * 0.3 upgrade done, significant upgrades to error handling

--- PipelinesToActions/PipelinesToActions/Controllers/HomeController.cs
+++ PipelinesToActions/PipelinesToActions/Controllers/HomeController.cs
@@ -10,6 +10,9 @@ using AzurePipelinesToGitHubActionsConverter.Core;
 
 namespace PipelinesToActionsWeb.Controllers
 {
+    /// <summary>
+    /// This controller follows the KISS principal. It's not always pretty, but HTTP server side posts was the quickest way to implement this functionality
+    /// </summary>
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -22,39 +25,51 @@ namespace PipelinesToActionsWeb.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            ConversionResult gitHubResult = new ConversionResult();
+            return View(viewName: "Index", model: gitHubResult);
         }
-
 
         [HttpPost]
         public IActionResult Index(string txtAzurePipelinesYAML)
         {
-            GitHubConversion gitHubResult;
+            ConversionResult gitHubResult;
             try
             {
                 Conversion conversion = new Conversion();
                 gitHubResult = conversion.ConvertAzurePipelineToGitHubAction(txtAzurePipelinesYAML);
+                gitHubResult.pipelinesYaml = txtAzurePipelinesYAML; //TODO: Move this into the Conversion module
             }
             catch (YamlDotNet.Core.YamlException ex)
             {
-                gitHubResult = new GitHubConversion();
-                gitHubResult.yaml = "Error processing YAML, it's likely the original YAML is not valid" + Environment.NewLine +
-                    "Original error message: " + ex.Message;
+                gitHubResult = new ConversionResult
+                {
+                    actionsYaml = "Error processing YAML, it's likely the original YAML is not valid" + Environment.NewLine +
+                    "Original error message: " + ex.Message,
+                    pipelinesYaml = txtAzurePipelinesYAML
+                };
             }
             catch (Exception ex)
             {
-                gitHubResult = new GitHubConversion();
-                gitHubResult.yaml = "Unknown error: " + ex.ToString();
+                gitHubResult = new ConversionResult
+                {
+                    actionsYaml = "Unexpected error: " + ex.ToString(),
+                    pipelinesYaml = txtAzurePipelinesYAML
+                };
             }
 
             //Return the result
             if (gitHubResult != null)
             {
-                return View(model: gitHubResult.yaml);
+                return View(model: gitHubResult);
             }
             else
             {
-                return View(model: "error");
+                gitHubResult = new ConversionResult
+                {
+                    actionsYaml = "Unknown error",
+                    pipelinesYaml = txtAzurePipelinesYAML
+                };
+                return View(model: gitHubResult);
             }
         }
 
@@ -70,10 +85,7 @@ jobs:
   build:
 
     runs-on: windows-latest
-    
-    env:
-      buildVersion: 1.0.0.0 #The initial build version, but this is updated below
-   
+      
     steps:
     # checkout the repo
     - uses: actions/checkout@v1
@@ -82,41 +94,31 @@ jobs:
     - name: Setup Dotnet for use with actions
       uses: actions/setup-dotnet@v1.0.0
     
-    #Build and test service   
+    #Build and test service  
     - name: Run automated unit and integration tests
-      run: dotnet test FeatureFlags/FeatureFlags.Tests/FeatureFlags.Tests.csproj --configuration Release --logger trx --collect ""Code coverage"" --settings:./FeatureFlags/FeatureFlags.Tests/CodeCoverage.runsettings
+      run: dotnet build MyProject/MyProject.Tests/MyProject.Service.csproj --configuration Release
+    - name: Run automated unit and integration tests
+      run: dotnet test MyProject/MyProject.Tests/MyProject.Tests.csproj --configuration Release
 
     #Publish dotnet objects
     - name: DotNET Publish Web Service
-      run: dotnet publish FeatureFlags/FeatureFlags.Service/FeatureFlags.Service.csproj --configuration Release 
-    - name: DotNET Publish Web Site
-      run: dotnet publish FeatureFlags/FeatureFlags.Web/FeatureFlags.Web.csproj --configuration Release 
-    - name: DotNET Publish functional tests
-      run: dotnet publish FeatureFlags/FeatureFlags.FunctionalTests/FeatureFlags.FunctionalTests.csproj --configuration Release
-    - name: Copy chromedriver for functional test
-      run: copy ""FeatureFlags/FeatureFlags.FunctionalTests/bin/Release/netcoreapp3.0/chromedriver.exe"" ""FeatureFlags/FeatureFlags.FunctionalTests/bin/Release/netcoreapp3.0/publish""
-      shell: powershell
+      run: dotnet publish MyProject/MyProject.Service/MyProject.Service.csproj --configuration Release 
     
     #Publish build artifacts to GitHub
     - name: Upload web service build artifacts back to GitHub
       uses: actions/upload-artifact@master
       with:
         name: serviceapp
-        path: FeatureFlags/FeatureFlags.Service/bin/Release/netcoreapp3.0/publish
-    - name: Upload website build artifacts back to GitHub
-      uses: actions/upload-artifact@master
-      with:
-        name: webapp
-        path: FeatureFlags/FeatureFlags.Web/bin/Release/netcoreapp3.0/publish
-    - name: Upload function test build artifacts back to GitHub
-      uses: actions/upload-artifact@master
-      with:
-        name: functionaltests
-        path: FeatureFlags/FeatureFlags.FunctionalTests/bin/Release/netcoreapp3.0/publish
+        path: MyProject/MyProject.Service/bin/Release/netcoreapp3.0/publish
 ";
 
-            return View(viewName: "Index", model: yaml);
+            ConversionResult gitHubResult = new ConversionResult
+            {
+                actionsYaml = yaml
+            };
+            return View(viewName: "Index", model: gitHubResult);
         }
+
         [HttpPost]
         public IActionResult CDExample()
         {
@@ -127,7 +129,7 @@ on: [push]
 
 jobs:
   #Deploy the artifacts to Azure
-  preDeploy:
+  deploy:
     runs-on: windows-latest
 
     needs: build
@@ -144,69 +146,23 @@ jobs:
       uses: actions/download-artifact@v1.0.0
       with:
         name: serviceapp
-    - name: Download webapp artifact
-      uses: actions/download-artifact@v1.0.0
-      with:
-        name: webapp
-    - name: Download functionaltests artifact
-      uses: actions/download-artifact@v1.0.0
-      with:
-        name: functionaltests
     
-    #Deploy service and website to Azure staging slots
+    #Deploy service to Azure staging slots
     - name: Deploy web service to Azure WebApp
       uses: Azure/webapps-deploy@v1
       with:
-        app-name: featureflags-data-eu-service
-        package: serviceapp
-        slot-name: staging     
-    - name: Deploy website to Azure WebApp
-      uses: Azure/webapps-deploy@v1
-      with:
-        app-name: featureflags-data-eu-web
-        package: webapp
-        slot-name: staging 
-
-    # Run functional tests on staging slots     
-    - name: Functional Tests
-      run: |
-        $vsTestConsoleExe = ""C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\Common7\\IDE\\Extensions\\TestPlatform\\vstest.console.exe""
-        $targetTestDll = ""functionaltests\FeatureFlags.FunctionalTests.dll""
-        $testRunSettings = ""/Settings:`""functionaltests\test.runsettings`"" ""
-        $parameters = "" -- TestEnvironment=""""Beta123""""  ServiceUrl=""""https://featureflags-data-eu-service-staging.azurewebsites.net/"""" WebsiteUrl=""""https://featureflags-data-eu-web-staging.azurewebsites.net/"""" ""
-        #Note that the `"" is an escape character to quote strings, and the `& is needed to start the command
-        $command = ""`& `""$vsTestConsoleExe`"" `""$targetTestDll`"" $testRunSettings $parameters "" 
-        Write-Host ""$command""
-        Invoke-Expression $command
-      shell: powershell
+        app-name: MyProject-data-eu-service
+        package: serviceapp  
   
-  #Deploy the artifacts to Azure
-  deploySwapSlots:
-    runs-on: ubuntu-latest # Note, Azure CLI requires a Linux runner...
-    
-    needs: [build, preDeploy]
-    #Only deploy if running off the master branch - we don't want to deploy off feature branches
-    if: github.ref == 'refs/heads/master'
-        
-    steps:
-    # Login with the secret SP details
-    - name: Log into Azure
-      uses: azure/login@v1
-      with:
-        creds: ${{ secrets.AZURE_SP }}     
-      #Swap staging slots with prod
-    - name: Swap web service staging slot to production
-      uses: Azure/cli@v1.0.0
-      with:
-        inlineScript: az webapp deployment slot swap --resource-group SamLearnsAzureFeatureFlags --name featureflags-data-eu-service --slot staging --target-slot production
-    - name: Swap web site staging slot to production
-      uses: Azure/cli@v1.0.0
-      with:
-        inlineScript: az webapp deployment slot swap --resource-group SamLearnsAzureFeatureFlags --name featureflags-data-eu-web --slot staging --target-slot production
 ";
 
-            return View(viewName: "Index", model: yaml);
+            ConversionResult gitHubResult = new ConversionResult
+            {
+                actionsYaml = yaml
+            };
+            return View(viewName: "Index", model: gitHubResult);
         }
+
         [HttpPost]
         public IActionResult CICDExample()
         {
@@ -219,9 +175,6 @@ jobs:
   build:
 
     runs-on: windows-latest
-    
-    env:
-      buildVersion: 1.0.0.0 #The initial build version, but this is updated below
    
     steps:
     # checkout the repo
@@ -233,17 +186,17 @@ jobs:
      
     #Build and test service   
     - name: Run automated unit and integration tests
-      run: dotnet test FeatureFlags/FeatureFlags.Tests/FeatureFlags.Tests.csproj --configuration Release --logger trx --collect ""Code coverage"" --settings:./FeatureFlags/FeatureFlags.Tests/CodeCoverage.runsettings
+      run: dotnet test MyProject/MyProject.Tests/MyProject.Tests.csproj --configuration Release 
 
     #Publish dotnet objects
     - name: DotNET Publish Web Service
-      run: dotnet publish FeatureFlags/FeatureFlags.Service/FeatureFlags.Service.csproj --configuration Release 
+      run: dotnet publish MyProject/MyProject.Service/MyProject.Service.csproj --configuration Release 
     - name: DotNET Publish Web Site
-      run: dotnet publish FeatureFlags/FeatureFlags.Web/FeatureFlags.Web.csproj --configuration Release 
+      run: dotnet publish MyProject/MyProject.Web/MyProject.Web.csproj --configuration Release 
     - name: DotNET Publish functional tests
-      run: dotnet publish FeatureFlags/FeatureFlags.FunctionalTests/FeatureFlags.FunctionalTests.csproj --configuration Release
+      run: dotnet publish MyProject/MyProject.FunctionalTests/MyProject.FunctionalTests.csproj --configuration Release
     - name: Copy chromedriver for functional test
-      run: copy ""FeatureFlags/FeatureFlags.FunctionalTests/bin/Release/netcoreapp3.0/chromedriver.exe"" ""FeatureFlags/FeatureFlags.FunctionalTests/bin/Release/netcoreapp3.0/publish""
+      run: copy ""MyProject/MyProject.FunctionalTests/bin/Release/netcoreapp3.0/chromedriver.exe"" ""MyProject/MyProject.FunctionalTests/bin/Release/netcoreapp3.0/publish""
       shell: powershell
     
     #Publish build artifacts to GitHub
@@ -251,17 +204,17 @@ jobs:
       uses: actions/upload-artifact@master
       with:
         name: serviceapp
-        path: FeatureFlags/FeatureFlags.Service/bin/Release/netcoreapp3.0/publish
+        path: MyProject/MyProject.Service/bin/Release/netcoreapp3.0/publish
     - name: Upload website build artifacts back to GitHub
       uses: actions/upload-artifact@master
       with:
         name: webapp
-        path: FeatureFlags/FeatureFlags.Web/bin/Release/netcoreapp3.0/publish
+        path: MyProject/MyProject.Web/bin/Release/netcoreapp3.0/publish
     - name: Upload function test build artifacts back to GitHub
       uses: actions/upload-artifact@master
       with:
         name: functionaltests
-        path: FeatureFlags/FeatureFlags.FunctionalTests/bin/Release/netcoreapp3.0/publish
+        path: MyProject/MyProject.FunctionalTests/bin/Release/netcoreapp3.0/publish
 
   #Deploy the artifacts to Azure
   preDeploy:
@@ -294,13 +247,13 @@ jobs:
     - name: Deploy web service to Azure WebApp
       uses: Azure/webapps-deploy@v1
       with:
-        app-name: featureflags-data-eu-service
+        app-name: MyProject-data-eu-service
         package: serviceapp
         slot-name: staging     
     - name: Deploy website to Azure WebApp
       uses: Azure/webapps-deploy@v1
       with:
-        app-name: featureflags-data-eu-web
+        app-name: MyProject-data-eu-web
         package: webapp
         slot-name: staging 
 
@@ -308,9 +261,9 @@ jobs:
     - name: Functional Tests
       run: |
         $vsTestConsoleExe = ""C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\Common7\\IDE\\Extensions\\TestPlatform\\vstest.console.exe""
-        $targetTestDll = ""functionaltests\FeatureFlags.FunctionalTests.dll""
+        $targetTestDll = ""functionaltests\MyProject.FunctionalTests.dll""
         $testRunSettings = ""/Settings:`""functionaltests\test.runsettings`"" ""
-        $parameters = "" -- TestEnvironment=""""Beta123""""  ServiceUrl=""""https://featureflags-data-eu-service-staging.azurewebsites.net/"""" WebsiteUrl=""""https://featureflags-data-eu-web-staging.azurewebsites.net/"""" ""
+        $parameters = "" -- TestEnvironment=""""Dev""""  ServiceUrl=""""https://MyProject-data-eu-service-staging.azurewebsites.net/"""" WebsiteUrl=""""https://MyProject-data-eu-web-staging.azurewebsites.net/"""" ""
         #Note that the `"" is an escape character to quote strings, and the `& is needed to start the command
         $command = ""`& `""$vsTestConsoleExe`"" `""$targetTestDll`"" $testRunSettings $parameters "" 
         Write-Host ""$command""
@@ -335,14 +288,17 @@ jobs:
     - name: Swap web service staging slot to production
       uses: Azure/cli@v1.0.0
       with:
-        inlineScript: az webapp deployment slot swap --resource-group SamLearnsAzureFeatureFlags --name featureflags-data-eu-service --slot staging --target-slot production
+        inlineScript: az webapp deployment slot swap --resource-group MyProjectRG --name MyProject-data-eu-service --slot staging --target-slot production
     - name: Swap web site staging slot to production
       uses: Azure/cli@v1.0.0
       with:
-        inlineScript: az webapp deployment slot swap --resource-group SamLearnsAzureFeatureFlags --name featureflags-data-eu-web --slot staging --target-slot production
+        inlineScript: az webapp deployment slot swap --resource-group MyProjectRG --name MyProject-data-eu-web --slot staging --target-slot production
 ";
-
-            return View(viewName: "Index", model: yaml);
+            ConversionResult gitHubResult = new ConversionResult
+            {
+                actionsYaml = yaml
+            };
+            return View(viewName: "Index", model: gitHubResult);
         }
 
         public IActionResult Privacy()
