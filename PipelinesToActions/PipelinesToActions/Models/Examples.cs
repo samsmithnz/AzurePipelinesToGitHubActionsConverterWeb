55695e08851a9912bed102a31f80aa6e2eecd49c
Sam Smith (Microsoft) <samsmithnz@gmail.com>
2020-03-28 09:13:52 -0400

    Added Maven functionality to website (#37)

--- PipelinesToActions/PipelinesToActions/Models/Examples.cs
+++ PipelinesToActions/PipelinesToActions/Models/Examples.cs
@@ -487,7 +487,23 @@ NodeExample Coming soon
         public static string MavenExample()
         {
             string yaml = @"
-MavenExample Coming soon
+trigger:
+- master
+
+pool:
+  vmImage: 'ubuntu-latest'
+
+steps:
+- task: Maven@3
+  inputs:
+    mavenPomFile: 'Maven/pom.xml'
+    mavenOptions: '-Xmx3072m'
+    javaHomeOption: 'JDKVersion'
+    jdkVersionOption: '1.8'
+    jdkArchitectureOption: 'x64'
+    publishJUnitResults: true
+    testResultsFiles: '**/surefire-reports/TEST-*.xml'
+    goals: 'package'
 ";
             return yaml;
         }
