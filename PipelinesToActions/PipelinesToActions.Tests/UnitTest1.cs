d5eba11d09ca372f915117dd489431d50d3e3345
Sam Smith <samsmithnz@gmail.com>
2022-12-13 10:25:27 -0500

    Adding sonar cloud and code coverage support (#208)
    * Added sonarcloud support
    
    * Added readme badges
    
    * upgraded package to .NET 7
    
    * Updated packages
    
    * updated workflow
    
    * Added tests project
    
    * Added code coverage
    
    * Fix to readme

---
+++ PipelinesToActions/PipelinesToActions.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace PipelinesToActions.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
